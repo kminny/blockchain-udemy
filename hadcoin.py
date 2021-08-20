a329bd23a56997c2c98a79e87990af46773eb4e8
kminny <darkdevil94@naver.com>
2021-08-20 16:42:11 +0900

    Add Transaction post request added

--- hadcoin.py
+++ hadcoin.py
@@ -144,6 +144,18 @@ def is_valid():
         reponse = {'message': 'Error, The Blockchain is not valid'}
     return jsonify(response), 200
 
+# Adding a new transactions to the Blockchain
+@app.route('/add_transaction', methods=['POST'])
+def add_transaction():
+    json = request.get_json()
+    transaction_keys = ['sender', 'receiver', 'amount']
+    if not all (key in json for key in transaction_keys):
+        return "Some elements of the transaction are missing", 400
+    index = blockchain.add_transaction(json['sender'], json['recevier'], json['amount'])
+    response = {'message': f'This transaction will be added to Block {index}'}
+    return jsonify(response), 201
+
+
 # Running the app
 app.run(host = '0.0.0.0', port = 5000)
 
