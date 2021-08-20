09d079c9b2772677c152b16d2466c89e488828f9
kminny <darkdevil94@naver.com>
2021-08-20 16:55:33 +0900

    Connect all node post request added

--- hadcoin.py
+++ hadcoin.py
@@ -155,8 +155,20 @@ def add_transaction():
     response = {'message': f'This transaction will be added to Block {index}'}
     return jsonify(response), 201
 
+# Part 3 - Decentrailizing our Blockchain
+@app.route('/connect_node', methods=['POST'])
+def connect_node():
+    json = request.get_json()
+    nodes = json.get('nodes')
+    if nodes is None:
+        return "No node", 400
+    for node in nodes:
+        blockchain.add_node(node)
+    response = {'message': 'All the nodes are now connected. The Hadcoin Blockchain now contains the following nodes',
+                'total_nodes': list(blockchain.nodes)}
+    return jsonify(response), 201
 
 # Running the app
 app.run(host = '0.0.0.0', port = 5000)
 
-# Part 3 - Decentrailizing our Blockchain
+
